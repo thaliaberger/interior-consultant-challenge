7e743d8ce29e1dbb3b1c64b70a1de7b9e95ba4f5
thaliaberger <thaliaberger@hotmail.com>
2021-02-15 23:33:47 -0300

    done

---
+++ index.js
@@ -0,0 +1,24 @@
+var toggles = document.querySelectorAll(".c-hamburger");
+
+for (var i = toggles.length - 1; i >= 0; i--) {
+  var toggle = toggles[i];
+  toggleHandler(toggle);
+}
+
+function toggleHandler(toggle) {
+  toggle.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (this.classList.contains("is-active") === true) {
+      this.classList.remove("is-active");
+      $(".open").removeClass("oppenned");
+    } else {
+      this.classList.add("is-active");
+      $(".open").addClass("oppenned");
+    }
+  });
+  console.log(ok);
+}
+$(".sub-menu li a").click(function (event) {
+  $(".open").removeClass("oppenned");
+  $(".c-hamburger").removeClass("is-active");
+});
